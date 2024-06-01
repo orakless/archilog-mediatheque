50f759d43aec37ac655d45030157e5a68abdaab2
Eva <orakle@tutamail.com>
2024-06-02 01:33:57 +0200

    server: docs: entitywrapper class documentation

--- server/src/entities/wrappers/EntityWrapper.java
+++ server/src/entities/wrappers/EntityWrapper.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 
+/***
+ * Le but de cette classe est de permettre l'encapsulation des données, et d'assurer qu'il n'y ait
+ * jamais d'utilisation concurrente "nocive" au sein d'une même ConcurrentMap.
+ * Normalement, une ConcurrentMap réalise déjà ce travail, mais nous avons besoin d'afficher les données
+ * de la map à certains moments (notamment pour lister les entités à l'utilisateur), sachant qu'il n'y
+ * a pas de moyen thread-safe d'itérer sur tous les éléments de la map dans les implémentations  (les informations récupérées
+ * pourraient ne pas être à jour).
+ * @param <T>
+ */
 public class EntityWrapper<T> {
     private ConcurrentMap<Integer, T> entities;
 
